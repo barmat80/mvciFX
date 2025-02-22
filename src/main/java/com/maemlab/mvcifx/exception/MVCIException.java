95ff23aace27291825557f1243297409f9a3bd8b
Mattia <mattia.bargellini@gmail.com>
2025-02-22 12:19:31 +0100

    Repackaging

--- src/main/java/com/maemlab/mvcifx/exception/MVCIException.java
+++ src/main/java/com/maemlab/mvcifx/exception/MVCIException.java
@@ -8,7 +8,36 @@ import com.maemlab.mvcifx.mvci.Interactor;
  *
  * <p>Users of Interactor interface can override constructors for custom behavior.
  *
- * @see com.maemlab.mvcifx.mvci.Interactor
+ * @see Interactor
  */
 public abstract class MVCIException extends Exception {
+    /**
+     * Constructs a new MVCI exception with the specified detail message.
+     *
+     * @param message the detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.
+     */
+    public MVCIException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new MVCI exception with the specified detail message and cause.
+     *
+     * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+     * @param err the cause (which is saved for later retrieval by the {@link #getCause()} method). A {@code null} value is permitted
+     * and indicates that the cause is nonexistent or unknown.
+     */
+    public MVCIException(String message, Throwable err) {
+        super(message, err);
+    }
+
+    /**
+     * Constructs a new MVCI exception with an empty message and the specified cause.
+     *
+     * @param e the cause (which is saved for later retrieval by the {@link #getCause()} method). A {@code null} value is permitted
+     *          and indicates that the cause is nonexistent or unknown.
+     */
+    public MVCIException(Exception e) {
+        super("", e);
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module com.maemlab.mvcifx {
     requires javafx.controls;
 
     exports com.maemlab.mvcifx.mvci;
+    exports com.maemlab.mvcifx.mvci.base;
     exports com.maemlab.mvcifx.exception;
 }
